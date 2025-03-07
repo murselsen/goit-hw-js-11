a8fb1efdfa35fd14ec22b69ae6788d526b3377ca
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-07 14:47:28 +0300

    bump version to 1.0.257, add SimpleLightbox for enhanced gallery functionality, and update styles for improved layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
+// Axios
 import axios from 'axios';
-// Dokümantasyonda belirtilen import
+// IziToast
 import iziToast from 'izitoast';
-// Stil importu
 import 'izitoast/dist/css/iziToast.min.css';
+// SimpleLightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.app-form');
 const gallery = document.querySelector('#appGallery');
@@ -11,35 +14,86 @@ const galleryList = document.querySelector('#galleryList');
 const galleryItem = photoInfo => {
   const item = document.createElement('li');
   item.classList.add('gallery-item');
+  item.dataset.source = photoInfo.largeImageURL;
+
+  const itemLink = document.createElement('a');
+  itemLink.href = photoInfo.largeImageURL;
+  itemLink.classList.add('gallery-link');
+  itemLink.style.color = 'black';
 
   const img = document.createElement('img');
   img.src = photoInfo.webformatURL;
   img.alt = photoInfo.tags;
   img.width = 360;
   img.height = 200;
-  img.dataset.source = photoInfo.largeImageURL;
 
   const contentDiv = document.createElement('div');
   contentDiv.classList.add('content');
 
   // Likes
-  const infoLikesDiv = document.createElement('div');
-  infoLikesDiv.classList.add('info');
+  const infoDivLikes = document.createElement('div');
+  infoDivLikes.classList.add('info');
 
-  const infoLikesKey = document.createElement('h5');
-  infoLikesKey.classList.add('key');
-  infoLikesKey.textContent = 'Likes';
-  infoLikesDiv.appendChild(infoLikesKey);
+  const infoKeyLikes = document.createElement('h5');
+  infoKeyLikes.classList.add('key');
+  infoKeyLikes.textContent = 'Likes';
+  infoDivLikes.appendChild(infoKeyLikes);
 
-  const infoLikesValue = document.createElement('p');
-  infoLikesValue.classList.add('value');
-  infoLikesValue.textContent = photoInfo.likes;
-  infoLikesDiv.appendChild(infoLikesValue);
+  const infoValueLikes = document.createElement('p');
+  infoValueLikes.classList.add('value');
+  infoValueLikes.textContent = photoInfo.likes;
+  infoDivLikes.appendChild(infoValueLikes);
 
-  contentDiv.appendChild(infoLikesDiv);
+  // Views
+  const infoDivViews = document.createElement('div');
+  infoDivViews.classList.add('info');
 
-  item.appendChild(img);
+  const infoKeyViews = document.createElement('h5');
+  infoKeyViews.classList.add('key');
+  infoKeyViews.textContent = 'Views';
+  infoDivViews.appendChild(infoKeyViews);
+
+  const infoValueViews = document.createElement('p');
+  infoValueViews.classList.add('value');
+  infoValueViews.textContent = photoInfo.views;
+  infoDivViews.appendChild(infoValueViews);
+
+  // Comments
+  const infoDivComments = document.createElement('div');
+  infoDivComments.classList.add('info');
+
+  const infoKeyComments = document.createElement('h5');
+  infoKeyComments.classList.add('key');
+  infoKeyComments.textContent = 'Comments';
+  infoDivComments.appendChild(infoKeyComments);
+
+  const infoValueComments = document.createElement('p');
+  infoValueComments.classList.add('value');
+  infoValueComments.textContent = photoInfo.views;
+  infoDivComments.appendChild(infoValueComments);
+
+  // Comments
+  const infoDivDownloads = document.createElement('div');
+  infoDivDownloads.classList.add('info');
+
+  const infoKeyDownloads = document.createElement('h5');
+  infoKeyDownloads.classList.add('key');
+  infoKeyDownloads.textContent = 'Downloads';
+  infoDivDownloads.appendChild(infoKeyDownloads);
+
+  const infoValueDownloads = document.createElement('p');
+  infoValueDownloads.classList.add('value');
+  infoValueDownloads.textContent = photoInfo.downloads;
+  infoDivDownloads.appendChild(infoValueDownloads);
+
+  contentDiv.appendChild(infoDivLikes);
+  contentDiv.appendChild(infoDivViews);
+  contentDiv.appendChild(infoDivComments);
+  contentDiv.appendChild(infoDivDownloads);
+
+  itemLink.appendChild(img);
   item.appendChild(contentDiv);
+  item.appendChild(itemLink);
   galleryList.appendChild(item);
 };
 
@@ -60,79 +114,22 @@ form.addEventListener('submit', async e => {
     .then(response => {
       console.log("Pixabay'den gelen veri", response);
       const photos = response.data.hits;
-      photos.forEach(photo => {
-        galleryItem(photo);
-      });
+      if (photos.length === 0) {
+        iziToast.error({
+          title: 'Veri Bulunamadı',
+          message: 'Illegal operation',
+        });
+      } else {
+        photos.forEach(photo => {
+          galleryItem(photo);
+        });
+        new SimpleLightbox('.gallery li > a', {
+          captionsData: 'alt',
+          captionDelay: 350,
+        });
+      }
     })
     .catch(error => {
       console.error("Pixabay'den gelen Hata", error);
     });
 });
-
-/* {
-collections
-:
-3614
-comments
-:
-33
-downloads
-:
-136280
-id
-:
-1400243
-imageHeight
-:
-2000
-imageSize
-:
-1180333
-imageWidth
-:
-3000
-largeImageURL
-:
-"https://pixabay.com/get/g771210c19e1526bf42ea01e9e76bd4d83b077a62987065aae9e12a92179c8b35df5a3ac4f61dd04f91a98e816f3d69c6ab6b38f719f8ddf22b6d332c2ac3e5f7_1280.jpg"
-likes
-:
-299
-pageURL
-:
-"https://pixabay.com/photos/buick-oldtimer-old-car-blue-car-1400243/"
-previewHeight
-:
-99
-previewURL
-:
-"https://cdn.pixabay.com/photo/2016/05/18/10/52/buick-1400243_150.jpg"
-previewWidth
-:
-150
-tags
-:
-"buick, oldtimer, old, car, blue car, classic, vintage, retro, car wallpapers, nostalgia, nostalgic, old car, parked car, car body, vehicle, transportation, automobile, automotive, car, car, car, car, car"
-type
-:
-"photo"
-user
-:
-"WildPixar"
-userImageURL
-:
-"https://cdn.pixabay.com/user/2023/01/25/09-56-36-827_250x250.png"
-user_id
-:
-2381951
-views
-:
-174316
-webformatHeight
-:
-426
-webformatURL
-:
-"https://pixabay.com/get/g037427fbf8523490aa0b79e063221976aad764539993cb9ce922fdc4035488ad242f442eb83164d2283b60d0b496b25407b0ef3aa70dfc3664ec05b0be406e1e_640.jpg"
-webformatWidth
-:
-640} */
